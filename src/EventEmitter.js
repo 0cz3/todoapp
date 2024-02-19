440cf2714fad82039cb0898ed6abf700dfa0dece
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-02-19 10:59:27 +0900

    イベントとモデル addEventListenerメソッドを定義

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,4 +1,17 @@
 export class EventEmitter {
     // 登録する[イベント名, Set(リスナー関数)]を管理するMap
     #listeners = new Map();
+    /**
+     * 指定したイベントが実行された時に呼び出されるリスナー関数を登録する
+     * @param {string} type イベント名
+     * @param {function} listener イベントリスナー
+     */
+    addEventListener(type, listener){
+        // 指定したイベントに対応するSetを作成しリスナー関数を登録する
+        if(!this.#listeners.has(type)){
+            this.#listeners.set(type, new Set());
+        }
+        const listenerSet = this.#listeners.get(type);
+        listenerSet.add(listener);
+    }
 }
