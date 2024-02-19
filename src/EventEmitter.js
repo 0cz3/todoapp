fc57ce5b631a09eaf4f5e8e23d3eb7eb940ebd3c
0cz3 <sumire.hamaguchi@nijibox.co.jp>
2024-02-19 12:37:09 +0900

    イベントとモデル removeEventListenerメソッドの追加

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -28,4 +28,22 @@ export class EventEmitter {
             listener.call(this);
         });
     }
+    /**
+     * 指定したイベントのイベントリスナーを解除する
+     * @param {string} type イベント名
+     * @param {Function} listener イベントリスナー
+     */
+    removeEventListener(type, listener) {
+        // 指定したイベントに対応するSetを取り出し、該当するリスナー関数を削除する
+        const listenerSet = this.#listeners.get(type)
+        if(!listenerSet){
+            return;
+        }
+        listenerSet.forEach(ownListener => {
+            if(ownListener === listener) {
+                listenerSet.delete(listener)
+            }
+        })
+
+    }
 }
